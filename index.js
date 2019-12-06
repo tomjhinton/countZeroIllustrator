71f3004aefd746e34f2cb24b54bb56f3078d5433
Tom Hinton <tomjhinton@gmail.com>
2019-12-06 18:32:53 +0000

    turn an led on and off

---
+++ index.js
@@ -0,0 +1,34 @@
+var app = require('http').createServer(handler),
+     io = require('socket.io').listen(app),
+     fs = require('fs'),
+   five = require('johnny-five');
+
+app.listen(8080)
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+let board = new five.Board();
+board.on("ready", function() {
+
+
+  let led = new five.Led(13);
+  
+
+  io.sockets.on('connection', function (socket) {
+    socket.on('click', function () {
+      console.log('clicked')
+      led.toggle();
+    });
+  });
+});
---
+++ src/app.js
@@ -0,0 +1,17 @@
+import 'bulma'
+import './style.scss'
+import '@babel/polyfill'
+
+
+const io = require('socket.io-client')
+
+var socket = io.connect('http://localhost:8080')
+
+
+
+const button = document.getElementById('button')
+
+button.addEventListener('click', function (e) {
+  socket.emit('click')
+  console.log('hiya')
+})
